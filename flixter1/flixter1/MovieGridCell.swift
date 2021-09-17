d6e5494fef6d70fe7b119103ceffba7c0d67fd8a
Nanae <42784268+nanaenolab@users.noreply.github.com>
2021-09-17 12:34:49 -0400

    flixter unit2
    superheropage

---
+++ flixter1/flixter1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter1
+//
+//  Created by nanae itoi on 2021/09/16.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
